5706f6f34689585c642ac6f2932611f7fd4900d9
hi-lee-mon <shunmel.09@gmail.com>
2024-08-03 15:09:52 +0900

    🏷️ supabaseのテーブルから型を生成

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,8 +1,9 @@
+import { Database } from '@/types/types_db'
 import { createBrowserClient } from '@supabase/ssr'
 
 // ドキュメントの内容をそのままコピーした(https://supabase.com/docs/guides/auth/server-side/nextjs?queryGroups=router&router=app)
 export function createClient() {
-  return createBrowserClient(
+  return createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
